32d1197e30e970c10822759e7dbebbf7597e69d5
Jouse then <jthen93@gmail.com>
2021-02-24 05:55:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,11 +12,11 @@ class Dog
     @@all
   end
   
-  def clear_all
+  def self.clear_all
     @@all.clear
   end
   
-  def print_all
+  def self.print_all
     @@all.each {|dog| puts @all.name}
   end
   

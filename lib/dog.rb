b31029fb3200501c31726b1d9095c9664d978fd7
Jouse then <jthen93@gmail.com>
2021-02-24 05:49:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Add your code here
+class Dog 
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @all << self
+  end 
+  
+  def all
+    @all
+  end
+  
+  def print_all
+    @@all.each {|dog| puts @all.name}
+  end
+end
